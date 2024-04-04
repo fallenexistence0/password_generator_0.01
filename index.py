11cfb3c2bd60ca96ac9e519b366481479cf2c6f2
fallenexistence0 <fallenexistence@proton.me>
2024-04-04 14:23:06 +0000

    Used the secrets library instead of the random library, as this makes it less predictable and more secure, also maed it so that generated passwords are hashed before being written to the csv file.

--- index.py
+++ index.py
@@ -1,28 +1,37 @@
-import random
+import secrets
 import string
 import csv
-
+import hashlib
 special_char = ['_','&','!','?',';','@','^']
 special_char = ''.join(special_char)
 allowed_char = string.ascii_uppercase + string.digits + string.ascii_lowercase + special_char
 
 
 def password_generator():
-    length = random.randint(20,50)
-    full_pass = ''.join(random.SystemRandom().choice(allowed_char) for _ in range(length))
+    length = secrets.randbelow(31) + 20
+    full_pass = ''.join(secrets.choice(allowed_char) for _ in range(length))
     return full_pass
+
+def hash_password(password) :
+    hasher = hashlib.sha256()
+    hasher.update(password.encode())
+    hashed_password = hasher.hexdigest()
+    return hashed_password
     
 with open('list.csv', 'w', newline='') as file:
     
     writer = csv.writer(file)
-    writer.writerow(['passwords'])
+    writer.writerow(['hashed_passwords'])
     
     iteration_num = range(0, 10)
     
     for i in iteration_num:
         password = password_generator()
+        hashed_password = hash_password(password)
+
+        print(hashed_password)
         print(password)
-        writer.writerow([password])
+        writer.writerow([hashed_password])
 
 
 
