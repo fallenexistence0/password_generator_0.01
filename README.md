fefa25825c45c12584f8dbda1ed4990f1c2f764e
fallenexistence0 <fallenexistence@proton.me>
2024-04-08 21:19:24 +0000

    Quick update

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # password_generator
 This is a small side project I am building, bit by bit.
 
-I plan to create a fully working password ganerator that I can use, features will be added gradually.
+I plan to create a fully working password generator that I can use, features will be added gradually.
 
-The entire script may change as I learn and dedicate more time to developing the project, for now I'm using the libraries: random, string and csv to generate a set of random strings, symbols and numbers, that are then written to a csv file.
+The entire script may change as I learn and dedicate more time towards the project, for now I am using the libraries: random, string and csv to generate a set of random strings, symbols and numbers, that are then written to a csv file.
 
 #Update 1: 
 
-Replaced the random module with the secrets, it seems that the random library is not so random afterall lol.
-Also made to that the generated password is hashed before being written to the csv file.
+Replaced the random module with the secrets, it seems that the random library is not so random afterall.
+Also made it so that the generated password is hashed before being written to the csv file.
 
 #Update 2:
 
 Shortened the number of generated passwords to just one.
-Added a while loop that lets the user decide if they want to includ special characters or not not.
+Added a while loop that lets the user decide if they want to includ special characters or not.
 Added a while loop that enforces a minimum of characters in a password (10 to be exact), although the way it is written now, the password function chooses a random number between 0 and 9, and adds whatever the user inputs, so if the user inputs the number 10, and the random number for the specific password is 5, the result will be 15 charcters.
 
 Updated the read me, as I though the comment I added when I commited the changes will update the read me (??) lol.
+
+#Update 3:
+
+Added some comments, and fixed some typos.
--- index.py
+++ index.py
@@ -5,6 +5,7 @@ import hashlib
 special_char = ['_','&','!','?',';','@','^']
 special_char = ''.join(special_char)
 
+# Loop that dictates the type of password to be generated.
 while True:
     x = int(input("Would you like to include special characters? (0, 1):"))
     if x == 0:
@@ -15,28 +16,30 @@ while True:
         break
     else:
         print("Invalid entry please try again.")
-    
+
+# Loop that enforces a minimum password lenght
 while True:
     password_lenght = int(input("Enter the number of characters in the password (this includes letters and symbols): "))
     if password_lenght < 10:
         print("The minimum password lenght is 10, please enter a valid entry")
     elif password_lenght >= 10:
         break
-    # This line is useless as entering qnything other thqn qn int in the password_lenght variable will end the script and give an error.
+    # This line is useless as entering anything other than an int in the password_lenght variable will end the script and give an error.
     else:
         print("Error invalid entry: Please enter a valid number.")
 
+# Functions that generates password
 def password_generator():
     length = secrets.randbelow(10) + password_lenght
     full_pass = ''.join(secrets.choice(allowed_char) for _ in range(length))
     return full_pass
-
+# Function that hashes the password
 def hash_password(password) :
     hasher = hashlib.sha256()
     hasher.update(password.encode())
     hashed_password = hasher.hexdigest()
     return hashed_password
-    
+# Password is written to csv.
 with open('list.csv', 'w', newline='') as file:
     
     writer = csv.writer(file)
@@ -45,7 +48,7 @@ with open('list.csv', 'w', newline='') as file:
     password = password_generator()
     hashed_password = hash_password(password)
     print('hashed password is: {} '.format(hashed_password))
-    print('unencrypted password is: {} '.format(password))
+    # print('unencrypted password is: {} '.format(password))
     writer.writerow([hashed_password])
 
 
